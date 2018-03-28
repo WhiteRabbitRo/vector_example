b5b924bbeb08e95e3da906f0b5bf5d48dbecd16e
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-03-28 19:25:52 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -55,7 +55,7 @@ std::size_t vector_t::capacity() const //память
 
 void vector_t::push_back(int value) //добавление элемента
 {
-	if(size_ == capacity_) capacity_ *= 2;
+	if(size_ == capacity_) capacity_ = capacity * 2;
 	if(size_ == 0) {
 		size_=size_+1;
 		elements_=new int[1];
@@ -73,6 +73,8 @@ void vector_t::push_back(int value) //добавление элемента
 		elements_[size_-1]=value;
 		delete[]elements_2;
 	}
+	size_++;
+	elements_ [size_-1] = value;
 }
 
 void vector_t::pop_back() //удаление элемента
