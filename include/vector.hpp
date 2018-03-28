6fec76d4125437f6a68eb58d42f7c206e1196bdb
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-03-28 19:46:52 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -9,19 +9,19 @@ private:
 public:
 	vector_t(); //конструктор
 	vector_t(vector_t const & other); //конструктор копирования
-	vector_t & operator =(vector_t const & other);
+	vector_t & operator =(vector_t const & other); //оператор присваивания с копированием
 	~vector_t(); //деструктор
 
 	std::size_t size() const; //возвращение закрытого поля size
-	std::size_t capacity() const;
+	std::size_t capacity() const; //возвращение закрытого поля capacity
 
 	void push_back(int value); //1 - проверка заполненности, 2 -увеличивает размер
-	void pop_back(); //
+	void pop_back(); //1 - удаление элементов, 2 - проверка capacity (уменьшаем в 2 раза размер, если элементов в 4 раза больше)
 
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
+	int & operator [](std::size_t index); //индексикация
+	int operator [](std::size_t index) const; //позволяет изменять значения
 
 	bool operator ==(vector_t const & other) const; //оператор сравнения
 };
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
+bool operator !=(vector_t const & lhs, vector_t const & rhs); //нахождение неравных массивов
