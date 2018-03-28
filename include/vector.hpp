f5fd680278be4b012e60c4fea4a23e07734c0130
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-03-28 16:13:02 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -4,24 +4,24 @@ class vector_t
 {
 private:
 	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+	std::size_t size_; //количество элементов
+	std::size_t capacity_; //выделенная память
 public:
-	vector_t();
+	vector_t(); //конструктор
 	vector_t(vector_t const & other);
 	vector_t & operator =(vector_t const & other);
-	~vector_t();
+	~vector_t(); //деструктор
 
-	std::size_t size() const;
+	std::size_t size() const; //возвращение закрытого поля size
 	std::size_t capacity() const;
 
-	void push_back(int value);
-	void pop_back();
+	void push_back(int value); //1 - проверка заполненности, 2 -увеличивает размер
+	void pop_back(); //
 
 	int & operator [](std::size_t index);
 	int operator [](std::size_t index) const;
 
-	bool operator ==(vector_t const & other) const;
+	bool operator ==(vector_t const & other) const; //оператор сравнения
 };
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs);
