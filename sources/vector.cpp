72d3e55d9a7ac208ccf780ebbaa37a89c3183374
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-03-28 16:52:25 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -7,7 +7,7 @@ vector_t::vector_t() //конструктор
 {
 	size_ = 0;
 	capacity_ = 0;
-	elemetns_ = nullptr;
+	elements_ = nullptr;
 }
 
 vector_t::vector_t(vector_t const & other) //конструктор копирования
@@ -20,7 +20,7 @@ vector_t::vector_t(vector_t const & other) //конструктор копиро
 
 vector_t & vector_t::operator =(vector_t const & other) //оператор присваивания с копированием
 {
-	if (elements_ != nullptr) delete[] elemets_;
+	if (elements_ != nullptr) delete[] elements_;
 	size_ = other.size_;
 	elements_ = new int[size_];
 	for (int i = 0; i < size_; i++) elements_[i] = other.elements_[i];
